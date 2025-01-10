c24c0cccec271b02f7664d4fddbb2cec0fcd19e6
Ramishwor sah <122091424+Ramishworsah@users.noreply.github.com>
2025-01-10 20:35:56 +0530

    done

--- simpleCode/simple.java
+++ simpleCode/simple.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class simple {
-
-    
-
-
 public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    int n;
+    System.out.println("enter the value from user:");
+    n=scanner.nextInt();
+    if(n%2==0){
+        System.out.println("number is even [num]");
+    }
+    else{
+        System.out.println("the number is not even");
+    }
     
-    System.out.println("welcome back java program:");
+    // System.out.println("welcome back java program:");
 }
 }
---
+++ simpleCode/varibale.java
@@ -0,0 +1,21 @@
+public class varibale {
+     //*==> in the java we have a instance variable , instance variable can be declare inside the class we can access the instance variable by using instance variable  */
+      int a=7;
+    public static void main(String[] args) {
+        //?==> variable is a container which hold the value
+        int num=10;
+        int num1=20;
+        System.out.println(num+num1);
+        //!in java we have two types of comment single comment and multiple comment
+        varibale obj=new varibale();
+      System.out.println("the instance variable is:  "+obj.a);
+
+
+    
+
+
+    }
+}
+
+//todo==> datatype in java , datatype is use to deccalre any variable name int,float,string,char,double,boolean etc
+
